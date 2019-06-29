6f901e79f0bb394873a0ceb2bf1e49f946806e9a
Steven Hicks <steven@simula.no>
2019-06-29 22:55:09 +0200

    added comments

--- src/evaluate.py
+++ src/evaluate.py
@@ -51,20 +51,25 @@ def save_confusion_matrix(path, confusion_matrix, labels=None):
 
 def read_submission(submission_path):
     results = {}
+    duplicates = {}
     lines = []
     with open(submission_path) as f:
         for row in csv.reader(f):
             lines.append(row)
             image_id = os.path.splitext(row[0].strip())[0].strip()
+
+            if image_id in results:
+                # print(f'Duplicate in prediction: { image_id }')
+                duplicates[image_id] = row[1].strip()
+                continue
+                
             results[image_id] = row[1].strip()
-    return np.array(lines), results
+    return duplicates, np.array(lines), results
 
 def read_gt(gt_path):
-
     gt_classes = {}
     gt_results = {}
     gt_classes_list = []
-
     with open(gt_path) as csv_data:
         class_id = 0
         for row in csv.reader(csv_data):
@@ -86,6 +91,7 @@ def evaluate_submission(submission_path):
 
     submission_attributes = os.path.basename(submission_filename).split('_')
     
+    # Extract run attributes
     team_name = submission_attributes[1]
     task_name = submission_attributes[2]
     run_id    = os.path.splitext('_'.join(submission_attributes[3:]))[0]
@@ -99,31 +105,51 @@ def evaluate_submission(submission_path):
     if not os.path.exists(team_result_path):
         os.makedirs(team_result_path)
     
-    submission_lines, prediction_results = read_submission(submission_path)
+    # Read submission file
+    duplicates, submission_lines, prediction_results = read_submission(submission_path)
 
+    print(f'The number of predicted images is: { len(submission_lines) }')
+    print(f'The number of unique predicted images is: { len(np.unique(submission_lines[:, 0])) }')
+    print(f'The number of duplicates is : { len(duplicates) }')
+
+    # Check if prediction line contains time information
     if len(submission_lines[0]) > 3:
         submission_times = np.array(submission_lines[:, 3], dtype=np.float32)
         average_time = np.mean(submission_times)
         minimum_time = np.min(submission_times)
         maximum_time = np.max(submission_times)
 
+    # Read the ground truth and extract classes
     gt_classes, gt_results = read_gt(GROUND_TRUTH_PATH)
     number_of_classes = len(gt_classes.items())
+
+    # Check for prediction tuples. If present, select the correct prediction for evaluation
+    if len(duplicates.items()) > 0:
+        for image_id, prediction in duplicates.items():
+            if not image_id in gt_results:
+                continue
+            if prediction == gt_results[image_id]:
+                prediction_results[image_id] = prediction
 
     y_pred, y_truth = [], []
     missing = []
 
+    # Initialize confusion matrix
     confusion_matrix = np.zeros((number_of_classes, number_of_classes), dtype=np.float32)
 
+    # iterate over ground truth
     for image_id, actual_class in gt_results.items():
-
-        actual_class_id = gt_classes[actual_class]
-
+        
+        # Check for missing predictions
         if image_id not in prediction_results:
             missing.append(image_id)
             continue
 
+        # Get class id for both the actual and predicted class 
+        actual_class_id = gt_classes[actual_class]
         predicted_class_id = gt_classes[prediction_results[image_id]]
+        
+        # Increment confusion matrix
         confusion_matrix[predicted_class_id, actual_class_id] += 1
 
         y_pred.append(predicted_class_id)
@@ -135,12 +161,18 @@ def evaluate_submission(submission_path):
     if len(y_truth) != len(y_pred):
         raise Exception('The number of predicted values is NOT equal to the ground truth!')
 
+    # Write missing predictions to file in team results
     if len(missing) != 0:
         print(f'Submission missing the prediction of { len(missing) } images!')
+        with open(os.path.join(team_result_path, 'missing_predictions.txt'), 'w') as f:
+            for image in missing:
+                f.write(f'{ image }\n')
 
+    # Save confusion matrix to team results
     save_confusion_matrix(os.path.join(team_result_path, f'confusion_matrix.csv'),
         confusion_matrix, labels=[ el[0] for el in sorted(gt_classes.items(), key=lambda x: x[1]) ])
 
+    # Calculate metrics for the individual classes
     FP = confusion_matrix.sum(axis=0) - np.diag(confusion_matrix)  
     FN = confusion_matrix.sum(axis=1) - np.diag(confusion_matrix)
     TP = np.diag(confusion_matrix)
@@ -150,6 +182,7 @@ def evaluate_submission(submission_path):
     F1   = metrics.f1_score(y_truth, y_pred, average=None)
     SPEC = TN / (TN + FP)
 
+    # Create directories for class specific metrics
     class_specific_metrics_path   = os.path.join(team_result_path, 'class_specific_metrics')
     class_specific_confusion_path = os.path.join(team_result_path, 'class_specific_confusion_matricies')
 
@@ -159,6 +192,7 @@ def evaluate_submission(submission_path):
     if not os.path.exists(class_specific_confusion_path):
         os.makedirs(class_specific_confusion_path)
 
+    # Save metrics for individual classes
     for class_name, class_id in gt_classes.items():
         
         save_confusion_matrix(os.path.join(class_specific_confusion_path, f'{ class_name }_confusion_matrix.csv'),
@@ -175,19 +209,18 @@ def evaluate_submission(submission_path):
             f.write(f'specificity,{ SPEC[class_id] }\n')
             f.write(f'f1,{ F1[class_id] }\n')
 
-    SPEC = {
-        'macro': np.mean(TN / (TN + FP)),
-        'micro': np.mean(sum(TN) / (sum(TN) + sum(FP)))
-    }
+    # Calculate specificity seperateley as it is not avaiable in Scikit-Learn
+    SPEC = { 'macro': np.mean(TN / (TN + FP)), 'micro': np.mean(sum(TN) / (sum(TN) + sum(FP))) }
 
+    # Calculate multi-class results using micro and macro averaging
     for average_technique in AVERAGING_TECHNIQUES:
 
-        ACC  = metrics.accuracy_score(y_truth, y_pred)
         F1   = metrics.f1_score(y_truth, y_pred, average=average_technique)
         PREC = metrics.precision_score(y_truth, y_pred, average=average_technique)
         REC  = metrics.recall_score(y_truth, y_pred, average=average_technique)
         MCC  = metrics.matthews_corrcoef(y_truth, y_pred)
         
+        # Save results to team results
         with open(os.path.join(team_result_path, f'{ average_technique }_average_metrics.csv'), 'w') as f:
             f.write(f'metric,value\n')
             f.write(f'true-positives,{ int(sum(TP)) }\n')
@@ -204,11 +237,12 @@ def evaluate_submission(submission_path):
                 f.write(f'average-time,{ average_time }\n')
                 f.write(f'minimum-time,{ minimum_time }\n')
                 f.write(f'maximum-time,{ maximum_time }\n')
-                
+        
+        # Save results to common results
         with open(os.path.join(RESULTS_DIRECTORY, f'all_{ average_technique }_average_metrics.csv'), 'a') as f:
             f.write(f'{ team_name },{ task_name },{ run_id }')
             f.write(f',{ int(sum(TP)) },{ int(sum(TN)) },{ int(sum(FP)) },{ int(sum(FN)) }')
-            f.write(f',{ PREC },{ REC },{ F1 },{ MCC }')
+            f.write(f',{ PREC },{ REC },{ SPEC[average_technique] },{ F1 },{ MCC }')
             if average_time is not None:
                 f.write(f',{ average_time },{ minimum_time },{ maximum_time }')
             f.write(f'\n')
@@ -217,7 +251,7 @@ if __name__ == '__main__':
 
     for average_technique in AVERAGING_TECHNIQUES:
         with open(os.path.join(RESULTS_DIRECTORY, f'all_{ average_technique }_average_metrics.csv'), 'w') as f:
-            f.write('team-name,task-name,run-id,true-positives,true-negatives,false-positives,false-negatives,precision,recall,f1,mcc,average-time,minimum-time,maximum-time\n')
+            f.write('team-name,task-name,run-id,true-positives,true-negatives,false-positives,false-negatives,precision,recall/sensitivity,specificity,f1,mcc,average-time,minimum-time,maximum-time\n')
 
     for submission_filename in os.listdir(SUBMISSIONS_DIRECTORY_PATH):
 

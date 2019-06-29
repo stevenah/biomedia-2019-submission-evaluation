d2a293eaae7f2866f75a2c85a154da4601146660
Steven Hicks <steven@simula.no>
2019-06-29 10:16:10 +0200

    changed evaluate script

--- src/evaluate.py
+++ src/evaluate.py
@@ -6,8 +6,6 @@ import random
 import warnings
 
 from sklearn import metrics
-from itertools import product
-from tabulate import tabulate
 
 import numpy as np
 
@@ -28,6 +26,8 @@ METRICS_SHORT = [
     'FDR', 'FOR', 'ACC', 'F1', 'MCC', 'BM', 'MK', 'RK', 'FPS_AVG', 'FPS_MIN'
 ]
 
+AVERAGING_TECHNIQUES = ['macro', 'micro']
+
 METRICS_LONG = [
     'True Positive / Hit / TP', 'True Negative / Correct Rejection / TN', 'False Positive / False Alarm / FP', 'False negative / Miss / FN',
     'Recall / Sensitivity / Hit rate / True Positive Rate / TPR', 'Specificity / True Negative Rate / TNR', 'Precision / Positive Predictive value / PPV',
@@ -37,8 +37,18 @@ METRICS_LONG = [
     'Minimum Processing Speed / Minimum Frames per Second / FPS Min.'
 ]
 
-def save_confusion_matrix(path, confusion_matrix):
-    np.savetxt(path, confusion_matrix.astype(int), fmt='%i', delimiter=',')
+def save_confusion_matrix(path, confusion_matrix, labels=None):
+    
+    confusion_list = confusion_matrix.astype(int).tolist()
+
+    if labels is not None:
+        for label_index, label in enumerate(labels):
+            confusion_list[label_index].insert(0, label)
+        confusion_list.insert(0, ['predicted/actual', *labels])
+
+    with open(path, 'w') as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerows(confusion_list)
 
 def evaluate_submission(submission_path):
 
@@ -96,8 +106,13 @@ def evaluate_submission(submission_path):
     if len(missing) != 0:
         print(f'Submission missing the prediction of { len(missing) } images!')
 
-    save_confusion_matrix(os.path.join(team_result_path, f'confusion_matrix.csv'), confusion_matrix)
+    save_confusion_matrix(os.path.join(team_result_path, f'confusion_matrix.csv'),
+        confusion_matrix, labels=[ el[0] for el in sorted(gt_classes.items(), key=lambda x: x[1]) ])
 
+    FP = confusion_matrix.sum(axis=0) - np.diag(confusion_matrix)  
+    FN = confusion_matrix.sum(axis=1) - np.diag(confusion_matrix)
+    TP = np.diag(confusion_matrix)
+    TN = confusion_matrix.sum() - (FP + FN + TP)
     PREC = metrics.precision_score(y_truth, y_pred, average=None)
     REC  = metrics.recall_score(y_truth, y_pred, average=None)
     F1   = metrics.f1_score(y_truth, y_pred, average=None)
@@ -114,12 +129,18 @@ def evaluate_submission(submission_path):
     for class_name, class_id in gt_classes.items():
         
         save_confusion_matrix(os.path.join(class_specific_confusion_path, f'{ class_name }_confusion_matrix.csv'),
-            binary_confusion_matrix(y_truth, y_pred, class_id))
+            binary_confusion_matrix(y_truth, y_pred, class_id), labels=[f'non-{ class_name }', class_name])
 
-        with open(os.path.join(class_specific_metrics_path, f'{ class_name }_metrics.csv'), 'a') as f:
-            f.write(f'{ PREC[class_id] },{ REC[class_id] },{ F1[class_id] }\n')
+        with open(os.path.join(class_specific_metrics_path, f'{ class_name }_metrics.csv'), 'w') as f:
+            f.write(f'true-positives,{ int(TP[class_id]) }\n')
+            f.write(f'true-negatives,{ int(TN[class_id]) }\n')
+            f.write(f'false-positives,{ int(FP[class_id]) }\n')
+            f.write(f'false-negatives,{ int(FN[class_id]) }\n')
+            f.write(f'precision,{ PREC[class_id] }\n')
+            f.write(f'recall,{ REC[class_id] }\n')
+            f.write(f'f1,{ F1[class_id] }\n')
 
-    for average_technique in ['macro', 'micro']:
+    for average_technique in AVERAGING_TECHNIQUES:
 
         ACC  = metrics.accuracy_score(y_truth, y_pred)
         F1   = metrics.f1_score(y_truth, y_pred, average=average_technique)
@@ -128,13 +149,39 @@ def evaluate_submission(submission_path):
         MCC  = metrics.matthews_corrcoef(y_truth, y_pred)
         
         with open(os.path.join(team_result_path, f'{ average_technique }_average_metrics.csv'), 'w') as f:
-            f.write(f'{ PREC },{ REC },{ F1 },{ MCC }\n')
+            f.write(f'true-positives,{ int(sum(TP)) }\n')
+            f.write(f'true-negatives,{ int(sum(TN)) }\n')
+            f.write(f'false-positives,{ int(sum(FP)) }\n')
+            f.write(f'false-negatives,{ int(sum(FN)) }\n')
+            f.write(f'precision,{ PREC }\n')
+            f.write(f'recall,{ REC }\n')
+            f.write(f'f1,{ F1 }\n')
+            f.write(f'mcc,{ MCC }\n')
 
+            if average_time is not None:
+                f.write(f'average-time,{ average_time }\n')
+                f.write(f'minimum-time,{ minimum_time }\n')
+                f.write(f'maximum-time,{ maximum_time }\n')
+                
+        with open(os.path.join(RESULTS_DIRECTORY, f'all_{ average_technique }_average_metrics.csv'), 'a') as f:
+            f.write(f'{ team_name },{ task_name },{ run_id }')
+            f.write(f',{ int(sum(TP)) },{ int(sum(TN)) },{ int(sum(FP)) },{ int(sum(FN)) }')
+            f.write(f',{ PREC },{ REC },{ F1 },{ MCC }')
+            if average_time is not None:
+                f.write(f',{ average_time },{ minimum_time },{ maximum_time }')
+            f.write(f'\n')
+        
 if __name__ == '__main__':
+
+    for average_technique in AVERAGING_TECHNIQUES:
+        with open(os.path.join(RESULTS_DIRECTORY, f'all_{ average_technique }_average_metrics.csv'), 'w') as f:
+            f.write('team-name,task-name,run-id,true-positives,true-negatives,false-positives,false-negatives,precision,recall,f1,mcc,average-time,minimum-time,maximum-time\n')
 
     for submission_filename in os.listdir(SUBMISSIONS_DIRECTORY_PATH):
 
         if not os.path.splitext(submission_filename)[1] == '.csv':
             continue
+
+        print(f'Evaluating { submission_filename }...')
 
         evaluate_submission(os.path.join(SUBMISSIONS_DIRECTORY_PATH, submission_filename))
